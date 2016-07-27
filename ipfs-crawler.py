f0fa7d1a8167e2dca96ad085196ea206a9503da2
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2016-07-27 15:39:24 +0200

    Tika working, now for search!

--- ipfs-crawler.py
+++ ipfs-crawler.py
@@ -1,14 +1,14 @@
 import ipfsApi
 api = ipfsApi.Client('127.0.0.1', 5001)
 
-import requests
-from tika import parser as tika_parser
+import subprocess
+import json
 
 hashes = [
+    'QmbyLYvJ43xyUmrU5A2Ye4ZiPHXWmB4j5nYRSpiTLBhbtn',
     'QmTyPtsw49JV1iFMwtvCVCFP1QwFL4cGsxz2ZtHZJyJY5F',
     'QmQ4EvvCrx2cjY3AWVosfeyy7aKCf3WSVrKomRf1wbvQnW'
 ]
-
 
 crawl_results = {}
 def add_result(resource_hash, data):
@@ -19,7 +19,7 @@ def add_result(resource_hash, data):
 
         def update_entry(name):
             data.update({
-                name: data[name].union(crawl_results[resource_hash][name])
+                name: list(set(data[name] + crawl_results[resource_hash][name]))
             })
 
         update_entry('names')
@@ -28,22 +28,16 @@ def add_result(resource_hash, data):
     crawl_results[resource_hash] = data
 
 
-def crawl_data(data):
+def crawl_data(resource_hash):
     # Identify file based on data
-    data = tika_parser.from_buffer(data)
-    print(data)
-    return data
 
+    ipfs_path = '/ipfs/{0}'.format(resource_hash)
+    result = subprocess.run(['tika', '-j', ipfs_path],
+        check=True, stdout=subprocess.PIPE, universal_newlines=True)
 
-def read_data(resource_hash):
-    # Read some data for determining metadata
-    from contextlib import closing
+    parsed_results = json.loads(result.stdout)
 
-    url = 'http://127.0.0.1:8080/ipfs/{0}'.format(resource_hash)
-    with closing(requests.get(url, stream=True)) as r:
-        data = r.raw.read()
-
-    return data
+    return parsed_results
 
 
 def crawl_hash(resource_hash, name=None, parent_hash=None):
@@ -65,20 +59,17 @@ def crawl_hash(resource_hash, name=None, parent_hash=None):
             resource_hash, name
         ))
 
-        data = read_data(resource_hash)
-
-        crawl_result = crawl_data(data)
+        crawl_result = crawl_data(resource_hash)
 
         stat = api.object_stat(resource_hash)
 
-        add_result(
-            resource_hash,
-            {
-                'names': set([name]),
-                'parents': set([resource_hash]),
-                'size': stat['CumulativeSize']
-            }
-        )
+        crawl_result.update({
+            'names': [name],
+            'parents': [resource_hash],
+            'size': stat['CumulativeSize']
+        })
+
+        add_result(resource_hash, crawl_result)
 
 
 def main():
