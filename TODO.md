db48cf61162a2ea59a10480e6f9d42d89df198ad
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2016-08-07 18:08:38 +0200

    Updated DHT sniffer, todo.

---
+++ TODO.md
@@ -0,0 +1,5 @@
+1. Implement proper concurrency (greenlets, perhaps)
+2. Implement timeouts (much stuff that can't be found)
+3. Index folders (name, size)
+4. Handle exceptions in data extraction
+5. Queues for pending, indexed and failure
--- dht-sniffer.sh
+++ dht-sniffer.sh
@@ -1,1 +1,4 @@
-ipfs log tail | jq -r 'if .event == "dhtSentMessage" and .message.type == "ADD_PROVIDER"  then .message.key else empty end'
+#!/bin/sh
+
+#ipfs log tail | jq -r 'if .event == "dhtSentMessage" and .message.type == "ADD_PROVIDER"  then .message.key else empty end'
+ipfs log tail | jq -r 'if .event == "handleAddProvider" then .key else empty end' | uniq
